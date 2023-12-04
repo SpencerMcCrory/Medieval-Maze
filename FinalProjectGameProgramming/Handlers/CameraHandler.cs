d17f2203ac54045b858c1f65c66d6e39d8c1805e
Spencer McCrory <imk0hasdangles@gmail.com>
2023-12-04 06:06:13 -0500

    Version 1.00 Added Level Map Images, Character animation, Wall Collision, Camera Movement, User Spawn point, spike detection, button detection and game completion detection.

---
+++ FinalProjectGameProgramming/Handlers/CameraHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectGameProgramming.Handlers
+{
+    public class CameraHandler
+    {
+        public System.Numerics.Vector2 Position { get; set; }
+        public float Zoom { get; set; }
+
+        public CameraHandler()
+        {
+            Position = System.Numerics.Vector2.Zero;
+            Zoom = 1;
+        }
+
+
+
+        public Matrix GetViewMatrix()
+        {
+            return Matrix.CreateTranslation(new Vector3(-Position, 0)) *
+                   Matrix.CreateScale(Zoom, Zoom, 1);
+        }
+
+
+    }
+}
